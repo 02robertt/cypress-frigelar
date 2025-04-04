83473fe26f44b16035b29e2cf91dccb4e157a648
robertfariasmartins7 <robertfariasmartins7@gmail.com>
2025-04-04 06:43:44 -0300

    hml primeiro commit

---
+++ cypress.config.js
@@ -0,0 +1,22 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    defaultCommandTimeout: 8000,
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    baseUrl: 'https://a7833275c1tst-store.occa.ocs.oraclecloud.com',
+    setupNodeEvents(on, config) {
+    },
+    video: true,
+      reporter: 'mochawesome',
+      reporterOptions: {
+        reportDir: 'cypress/results',
+        overwrite: false,
+        html: true,
+        json: false,
+        timestamp: "mmddyyyy_HHMMss"
+    },
+   
+  }
+});
---
+++ cypress/e2e/homepage.cy.js
@@ -0,0 +1,32 @@
+describe('homepage', () => {
+  beforeEach(() => {
+    cy.visit('https://a7833275c1tst-store.occa.ocs.oraclecloud.com')
+    cy.window().should('have.property', 'document').its('readyState').should('eq', 'complete');
+    cy.get('#modalPrivacyPolicy-btn').click()
+  });
+  context('Validar no header se o logo, barra de busca e menu exibem na página', () => {
+    it('Verificacao do logo', () => {
+      cy.get('.logo-desktop > a > img').should('be.visible')
+  });
+    it('Verificacao da barra de pesquisa', () => {
+      cy.get('#search-input').should('be.visible')
+    });
+    it.only('Verificacao do menu', () => {
+      cy.get('.top-items-list').should('be.visible')
+      cy.get(':nth-child(2) > .dropdown-toggle').should('be.visible')
+      cy.get(':nth-child(4) > .dropdown-toggle').should('be.visible')
+      cy.get(':nth-child(6) > .dropdown-toggle').should('be.visible')
+      cy.get(':nth-child(8) > .dropdown-toggle').should('be.visible')
+      cy.get(':nth-child(10) > .dropdown-toggle').should('be.visible')
+      cy.get(':nth-child(12) > .dropdown-toggle').should('be.visible')
+      cy.get(':nth-child(14) > .dropdown-toggle').should('be.visible')
+      cy.get(':nth-child(16) > .dropdown-toggle').should('be.visible')
+      cy.get(':nth-child(18) > .dropdown-toggle').should('be.visible')
+    });
+    context('Validar o carrossel de banners principal', () => {
+      
+    });
+  
+  });
+
+  })
